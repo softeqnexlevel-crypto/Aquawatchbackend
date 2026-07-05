47a016b53711b4d9ebfa5f452437049578ebda59
Evansrintari <rintari110@gmail.com>
2026-07-05 18:29:20 +0300

    server

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 
 const corsOptions = {
     origin: process.env.NODE_ENV === 'production'
-        ? ['https://your-frontend-domain.com', 'https://aquawatch-flax-nine.vercel.app/']
+        ? ['https://your-frontend-domain.com', 'https://aquawatch-flax-nine.vercel.app']
         : ['http://localhost:3000', 'http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
