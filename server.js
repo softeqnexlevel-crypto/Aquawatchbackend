30a073bc6aa23bbc995345531bd272b852a87482
Evansrintari <rintari110@gmail.com>
2026-07-13 19:04:19 +0300

    added new url

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 // ==================== CORS CONFIG ====================
 const allowedOrigins = [
     'https://aquawatch-flax-nine.vercel.app',
-    'https://www.aquasystemtech.co.ke',
+    'https://aquasystemtech.co.ke',
     'http://localhost:3000',
     'http://localhost:5173',
     'http://localhost:3001'
